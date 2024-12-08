78f9f0cd96ebe5394c8bafc44d12b0bab964cf80
Jarmo <jarmovdh@gmail.com>
2024-12-08 16:24:07 +0100

    update config

--- 411-storefront/.eslintrc.js
+++ 411-storefront/.eslintrc.js
@@ -2,4 +2,5 @@ module.exports = {
   extends: ["next/core-web-vitals"],
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: ["@babel/plugin-proposal-class-properties"],
+  ignorePatterns: ["sanity/**/*"],
 }
